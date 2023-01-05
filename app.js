6e7750db95d911a92d11df2068d0bcb15b17d1a3
Henri <henri-joel.mugu@voco.ee>
2023-01-05 16:47:28 +0200

    let and const

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('yo yo n')
+
+
+//let, const
+let nr = 5 //primitive value
+// funktsioonid ja muud - const
+
+const numbers = [1, 2, 3]
+/*
+numbers [2] = 4
+console.log(numbers)
+console.log(numbers[0])
+console.log(numbers.length)
+*/
+
+//massiivi t66tlus - for each tsykkel
+/*
+numbers.forEach((numbers) => {
+    console.log(number, index)
+})
+*/
+/*
+const person = {
+    vanus: 5,
+    hobid: ['jalgpall', 'telekas', 'kass'],
+
+} */
+
+const person = {
+    name: 'Karl',
+    vanus: 25,
+
+}
+
+console.log(person)  //person.name, person.vanus voib ka panna sulgudesse
+
